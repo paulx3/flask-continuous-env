9cfe68b66e5cc334b0fea24682bc19a500b79a9c
paulx <xzycxy@126.com>
2017-07-07 17:11:54 +0800

    test4

---
+++ test.py
@@ -0,0 +1,18 @@
+'''
+
+@author: wanzeyu
+
+@contact: wan.zeyu@outlook.com
+
+@file: test.py.py
+
+@time: 2017/7/7 17:10
+
+@desc:
+
+'''
+import unittest
+
+
+class Test01(unittest.TestCase):
+    pass
